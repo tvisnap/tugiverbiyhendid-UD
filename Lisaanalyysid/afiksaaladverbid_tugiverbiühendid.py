b0a22e50f242db11fbb4b32277f4fc84aba657bb
Tanel Visnap <tanel.visnap@gmail.com>
2026-08-18 14:34:01 +0300

    Add files via upload

---
+++ Lisaanalyysid/afiksaaladverbid_tugiverbiühendid.py
@@ -0,0 +1,42 @@
+import csv
+from pyconll.conllu import conllu
+
+csv_fail = "tugiverbid_leiud_FINAL.csv"
+conllu_fail = "tugiverbid_kandidaatlaused_FINAL.conllu"
+valjund_fail = "afiksaaladverbid_tulemused.csv"
+
+# Laused ID järgi
+laused = {}
+
+for lause in conllu.load_from_file(conllu_fail):
+    lause_id = lause.meta["sent_id"]
+    laused[lause_id] = lause
+
+leiud = []
+
+# Vaatan iga kandidaadi kandidaatverbi
+with open(csv_fail, encoding="utf-8") as f:
+    for rida in csv.DictReader(f, delimiter=";"):
+
+        lause = laused[rida["Lause_ID"]]
+        verbi_id = rida["Verbi token"]
+
+        tokenid = {}
+
+        for token in lause.tokens:
+            tokenid[token.id] = token
+
+        verb = tokenid[verbi_id]
+
+        # Otsin kandidaatverbiga compound:prt-seoses komponendi
+        for token in lause.tokens:
+            if token.head == verbi_id and token.deprel == "compound:prt":
+                leiud.append((rida["Lause_ID"], token.lemma, verb.lemma))
+
+with open(valjund_fail, "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f, delimiter=";")
+    writer.writerow(["Lause_ID", "Afiksaaladverb", "Verb"])
+    writer.writerows(leiud)
+
+print("Kokku:", len(leiud))
+print("Väljundfail:", valjund_fail)
